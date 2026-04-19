69637463da955451f95876bda48ac54d0866cbcc
a-kouki <koukitech5@gmail.com>
2026-04-19 16:03:20 -0300

    Update protect-admin.ts

--- app/lib/protect-admin.ts
+++ app/lib/protect-admin.ts
@@ -8,8 +8,8 @@ export async function protectAdmin(req: NextRequest) {
   let response = NextResponse.next({ request: req })
 
   const supabase = createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_PUBLISHABLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll: () => req.cookies.getAll(),
