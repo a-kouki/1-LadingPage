ffeba148eb7b5f72089f3568d4572333d555f13a
kouki <koukitech5@gmail.com>
2026-04-19 11:46:52 -0300

    fix: corrigindo route.ts

--- scripts/rate-limit.js
+++
@@ -1,8 +0,0 @@
-async function testeLimit(){
-    for(let i=0 ; i < 30; i++){
-        const res = await fetch("http://localhost:3000/");
-        console.log(i, res.status)
-    }
-}
-
-testeLimit()
